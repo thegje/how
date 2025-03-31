62e8c988243828e382f5d6ec862108e2d6840162
root <root@330860.vps-10.com>
2025-03-31 20:38:02 +0000

    prepped for publish to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 
 setup(
-    name="how",
-    version="0.1",
-    scripts=["how.py"],
-    install_requires=["requests"],
+    name="how",                     # Package name (must be unique on PyPI)
+    version="0.1.0",                # Initial version
+    author="thegje",                # Your GitHub username or name
+    author_email="thegje@gmail.com", # Your email
+    description="A terminal app to query xAI's Grok API",
+    long_description=open("README.md").read(),  # Use README as description
+    long_description_content_type="text/markdown",  # Markdown format
+    url="https://github.com/thegje/how",  # GitHub repo URL
+    py_modules=["how"],             # Single module (how.py)
+    install_requires=["requests"],  # Dependency
     entry_points={
         "console_scripts": [
-            "how = how:main"  # 'howdo' command runs main()
+            "how = how:main"        # Command 'how' runs how.main()
         ]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.6",        # Minimum Python version
 )
